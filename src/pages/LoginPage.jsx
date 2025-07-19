b7130a1f0904ad6e1010928ac1028e5edd7ddfcc
HadiHD17 <hadihaidar1723@gmail.com>
2025-07-19 18:04:58 +0300

    finished linking for auth

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,52 +1,65 @@
-import React, {useState} from 'react'
-import "../styles/Login.css"
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState } from "react";
+import "../styles/Login.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const LoginPage = () => {
-    const [email, setemail] = useState("");
-    const [password, setpassword] = useState("");
-    const navigate = useNavigate();
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [loginError, setLoginError] = useState("");
+  const navigate = useNavigate();
 
-    const Login = (e) => {
-        e.preventDefault();
-        
-        const formdata = new FormData();
-        formdata.append("email", email);
-        formdata.append("password", password);
+  const Login = async (e) => {
+    e.preventDefault();
+    setLoginError("");
 
-        //for(let pair of formdata.entries()){
-          //  console.log(pair[0] + ": " + pair[1]);
-        //}
-       if(formdata){ 
-          navigate("/Dashboard");
-        }
+    const formdata = new FormData();
+    formdata.append("email", email);
+    formdata.append("password", password);
+    try {
+      const res = await axios.post(
+        "http://127.0.0.1:8000/api/v0.1/guest/login",
+        formdata
+      );
+      if (res.data?.status == "success") {
+        localStorage.setItem("token", res.data.payload.token);
+        navigate("/Dashboard");
+      }
+      setLoginError("Invalid email or password.");
+    } catch (error) {
+      setLoginError("Server error. Please try again later.");
+      console.error("Login error:", error);
     }
+  };
   return (
-    <div className='LoginForm'>
-        <h1>Welcome Back!</h1>
-        <form onSubmit={Login}>
-            <input 
-            type="email"
-            id="email"
-            className="email"
-            placeholder="enter your email"
-            onChange={(e) => setemail(e.target.value)}
-            required />
+    <div className="LoginForm">
+      <h1>Welcome Back!</h1>
+      <form onSubmit={Login}>
+        <input
+          type="email"
+          id="email"
+          className="email"
+          placeholder="enter your email"
+          onChange={(e) => setemail(e.target.value)}
+          required
+        />
 
-            <input 
-            type="password"
-            id="password"
-            className="password"
-            placeholder="enter your password"
-            onChange={(e) => setpassword(e.target.value)}
-            required />
+        <input
+          type="password"
+          id="password"
+          className="password"
+          placeholder="enter your password"
+          onChange={(e) => setpassword(e.target.value)}
+          required
+        />
 
-            <button type="submit" className="loginbtn" >Login</button>
-
-        </form>
+        <button type="submit" className="loginbtn">
+          Login
+        </button>
+        {loginError && <p className="error-text">{loginError}</p>}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,71 +1,87 @@
-import React, {useState} from 'react'
-import "../styles/Register.css"
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import "../styles/Register.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const RegisterPage = () => {
-    const [fullname, setfullname] = useState("");
-    const [email, setemail] = useState("");
-    const [password, setpassword] = useState("");
-    const [confirmpassword, setconfirmpassword] = useState("");
-    const navigate = useNavigate();
+  const [fullname, setfullname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [confirmpassword, setconfirmpassword] = useState("");
+  const [loginError, setLoginError] = useState("");
+  const navigate = useNavigate();
 
-    const Register = (e) => {
-        e.preventDefault();
-        
-        const formdata = new FormData();
-        formdata.append("fullname", fullname);
-        formdata.append("email", email);
-        formdata.append("password", password);
-        formdata.append("confirmpassword", confirmpassword);
+  const Register = async (e) => {
+    e.preventDefault();
+    setLoginError("");
 
-        //for(let pair of formdata.entries()){
-          //  console.log(pair[0] + ": " + pair[1]);
-        //}
-        if(formdata){
-          navigate("/Dashboard");
-        }
+    const formdata = new FormData();
+    formdata.append("fullname", fullname);
+    formdata.append("email", email);
+    formdata.append("password", password);
+    formdata.append("confirmpassword", confirmpassword);
+
+    try {
+      const res = await axios.post(
+        "http://127.0.0.1:8000/api/v0.1/guest/register",
+        formdata
+      );
+      if (res.data?.status == "success") {
+        localStorage.setItem("token", res.data.payload.token);
+        navigate("/Dashboard");
+      }
+      setLoginError("Invalid email or password.");
+    } catch (error) {
+      setLoginError("Server error. Please try again later.");
+      console.error("Login error:", error);
     }
+  };
   return (
     <div className="RegisterForm">
-        <h1>CREATE ACCOUNT</h1>
-        <form onSubmit={Register}>
-            <input 
-            type="text"
-            id="fullname"
-            className="fullname"
-            placeholder="enter your full name"
-            onChange={(e) => setfullname(e.target.value)}
-            required />
+      <h1>CREATE ACCOUNT</h1>
+      <form onSubmit={Register}>
+        <input
+          type="text"
+          id="fullname"
+          className="fullname"
+          placeholder="enter your full name"
+          onChange={(e) => setfullname(e.target.value)}
+          required
+        />
 
-            <input 
-            type="email"
-            id="email"
-            className="email"
-            placeholder="enter your email"
-            onChange={(e) => setemail(e.target.value)}
-            required />
+        <input
+          type="email"
+          id="email"
+          className="email"
+          placeholder="enter your email"
+          onChange={(e) => setemail(e.target.value)}
+          required
+        />
 
-            <input 
-            type="password"
-            id="password"
-            className="password"
-            placeholder="enter your password"
-            onChange={(e) => setpassword(e.target.value)}
-            required />
+        <input
+          type="password"
+          id="password"
+          className="password"
+          placeholder="enter your password"
+          onChange={(e) => setpassword(e.target.value)}
+          required
+        />
 
-            <input 
-            type="password"
-            id="confirmpassword"
-            className="confirmpassword"
-            placeholder="confirm your password"
-            onChange={(e) => setconfirmpassword(e.target.value)}
-            required />
+        <input
+          type="password"
+          id="confirmpassword"
+          className="confirmpassword"
+          placeholder="confirm your password"
+          onChange={(e) => setconfirmpassword(e.target.value)}
+          required
+        />
 
-            <button type="submit" className="registerbtn" >Sign Up</button>
-
-        </form>
+        <button type="submit" className="registerbtn">
+          Sign Up
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterPage
+export default RegisterPage;
